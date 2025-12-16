2b00014532861349e7d2d6a3d95f2187a63a3ae0
Abishan <abishanjeyarajah@gmail.com>
2025-12-16 12:02:12 +0530

    create a counter component using useState

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Abishan from "./Abishan.jsx";
 import App from "./App.jsx";
+import Counter from "./components/state/Counter.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <Abishan fname="Abishan" lname="Jeyarajah" id="1" />
-    <Abishan fname="Thubeerna" lname="Jeyarajah" id="2" />
-    <Abishan fname="Mahilini" lname="Jeyarajah" id="3" />
+  < Counter />
   </StrictMode>
 );
